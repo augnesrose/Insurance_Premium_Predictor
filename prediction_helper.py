2bf3ab897d7c1a1a09077b570df46e2e96800edd
Augnes2002 <augnesrose@gmail.com>
2025-09-29 16:00:51 +0530

    after rectifying error3

--- prediction_helper.py
+++ prediction_helper.py
@@ -3,10 +3,10 @@
 import pandas as pd
 import joblib
 
-model_young = joblib.load("artifacts\model_young.joblib")
-model_rest = joblib.load("artifacts\model_elder.joblib")
-scaler_young = joblib.load("artifacts\scaler_young.joblib")
-scaler_rest = joblib.load("artifacts\scaler_elder.joblib")
+model_young = joblib.load("artifacts/model_young.joblib")
+model_rest = joblib.load("artifacts/model_elder.joblib")
+scaler_young = joblib.load("artifacts/scaler_young.joblib")
+scaler_rest = joblib.load("artifacts/scaler_elder.joblib")
 
 def calculate_normalized_risk(medical_history):
     risk_scores = {
