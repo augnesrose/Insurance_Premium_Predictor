d68f08ea991d4d89c5b782ac86d8c62bd9f77c6c
Augnes2002 <augnesrose@gmail.com>
2025-09-29 15:53:25 +0530

    after rectifying error

--- prediction_helper.py
+++ prediction_helper.py
@@ -1,7 +1,7 @@
 # codebasics ML course: codebasics.io, all rights reserverd
 
 import pandas as pd
-import joblib
+from joblib import load
 
 model_young = joblib.load("artifacts\model_young.joblib")
 model_rest = joblib.load("artifacts\model_elder.joblib")
